bee2bd1caabafeb2103a9aaf4803a21771439fd1
minhnp2 <minhnp2@fa23-cs425-6801.cs.illinois.edu>
2023-12-01 19:43:15 -0600

    Fix script logic

--- sdfs/executors/wordcountmap.py
+++ sdfs/executors/wordcountmap.py
@@ -1,33 +1,38 @@
 import sys
 import re
 
-def map_words(input_file, output_prefix):
-    # Open the input file
-    with open(input_file, 'r') as file:
-        text = file.read()
-
-    # Split text into words using regular expression
-    words = re.findall(r'\w+', text.lower())
-
+def map_words(input_files, output_prefix):
     # Create a dictionary to count occurrences of each word
     word_count = {}
-    for word in words:
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
+
+    # Open the input files
+    for input_file in input_files:
+        with open(input_file, 'r') as file:
+            text = file.read()
+
+        # Split text into words using regular expression
+        words = re.findall(r'\w+', text.lower())
+
+        for word in words:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
 
     # Create output files for each word
     for word, count in word_count.items():
         with open(f'{output_prefix}_{word}', 'w') as file:
             file.write(f'{word}\t{count}\n')
 
+    for word in word_count.keys():
+        print(word)
+
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python map.py <input_file> <output_prefix>")
+    if len(sys.argv) < 3:
+        print("Usage: python map.py <output_prefix> [input_files]")
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    output_prefix = sys.argv[2]
+    output_prefix = sys.argv[1]
+    input_files = sys.argv[2:]
 
-    map_words(input_file, output_prefix)
+    map_words(input_files, output_prefix)
--- sdfs/executors/wordcountreduce.py
+++ sdfs/executors/wordcountreduce.py
@@ -1,13 +1,14 @@
 import sys
 
-def reduce_words(input_file, destination_file):
+def reduce_words(input_files, destination_file):
     # Read the input file
-    with open(input_file, 'r') as file:
-        lines = file.readlines()
+    for input_file in input_files:
+        with open(input_file, 'r') as file:
+            lines = file.readlines()
 
-    # Assume the format is 'word\tcount\n'
-    word, count = lines[0].strip().split('\t')
-    total_count = int(count)
+        # Assume the format is 'word\tcount\n'
+        word, count = lines[0].strip().split('\t')
+        total_count = int(count)
 
     # Write the result to the destination file
     with open(destination_file, 'w') as file:
@@ -18,7 +19,7 @@ if __name__ == "__main__":
         print("Usage: python reduce.py <input_file> <destination_file>")
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    destination_file = sys.argv[2]
+    destination_file = sys.argv[1]
+    input_files = sys.argv[2:]
 
-    reduce_words(input_file, destination_file)
+    reduce_words(input_files, destination_file)
